83e6f4dc4d64ccc07e88ec87c578c48c21614fce
srivastr <srivastr@Srivastr-E6540.corp.root.ipc.com>
2017-08-24 16:00:09 -0400

    Updated

--- demoMongo/src/test/java/com/mars/AbstractTest.java
+++ demoMongo/src/test/java/com/mars/AbstractTest.java
@@ -5,6 +5,11 @@ import org.junit.rules.ExpectedException;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import com.mars.models.Login;
+import com.mars.mongo.repository.entity.Customer;
+import com.mars.mongo.repository.entity.Role;
+import com.mars.mongo.repository.entity.User;
+
 
 public abstract class AbstractTest
 {
@@ -14,5 +19,41 @@ public abstract class AbstractTest
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
- 
+    protected User composeUserModel()
+    {
+        User model = new User();
+        model.setId("1");
+        model.setFirstName("FN");
+        model.setLastName("LN");
+        model.setEmail("test@mars.com");
+        Role roleModel = new Role();
+        roleModel.setId("2");
+        roleModel.setName("role");
+        model.setRole(roleModel);
+        Customer companyModel = new Customer();
+        companyModel.setId("1000");
+        companyModel.setName("company");
+        model.setCompany(companyModel);
+        model.setPassword("some password");
+        return model;
+    }
+
+    protected Customer getCompanyModel()
+    {
+        Customer company = new Customer();
+        company.setId("1001");
+        company.setName("Mars-TEST");
+        company.setEmailDomain("@mars.com");
+        
+
+        return company;
+    }
+
+    protected Login getLoginModel()
+    {
+        Login login = new Login();
+        login.setEmail("test@mars.com");
+        login.setPassword("WElcome12#");
+        return login;
+    }
 }
